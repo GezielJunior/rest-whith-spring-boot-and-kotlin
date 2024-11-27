0d26f5d8a17b8e86d508c61afb3d5020fd574471
Geziel Junior <geziel.junior@ipremios.com>
2024-11-27 20:15:01 -0300

    First Step in kotlin with Spring boot

---
+++ rest-whith-spring-boot-and-kotlin/src/main/kotlin/br/com/devwork/rest_whith_spring_boot_and_kotlin/Greeting.kt
@@ -0,0 +1,3 @@
+package br.com.devwork.rest_whith_spring_boot_and_kotlin
+
+class Greeting (val id: Long, val content: String) {}
---
+++ rest-whith-spring-boot-and-kotlin/src/test/kotlin/br/com/devwork/rest_whith_spring_boot_and_kotlin/StartupTests.kt
@@ -0,0 +1,13 @@
+package br.com.devwork.rest_whith_spring_boot_and_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
